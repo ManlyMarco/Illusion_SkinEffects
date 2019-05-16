3125d01ac32e494f5cd020189bc1a0c9f62971ac
ManlyMarco <dummyaccx@gmail.com>
2019-05-16 02:36:55 +0200

    Version up to 1.5

--- KK_SkinEffects/SkinEffectsMgr.cs
+++ KK_SkinEffects/SkinEffectsMgr.cs
@@ -11,7 +11,7 @@ namespace KK_SkinEffects
     internal partial class SkinEffectsMgr : BaseUnityPlugin
     {
         public const string GUID = "Marco.SkinEffects";
-        internal const string Version = "1.4";
+        internal const string Version = "1.5";
 
         [DisplayName("!Enable virgin bleeding")]
         [Description("When penetrated for the first time, virgins have a chance to bleed. The extent varies based on their status." +
