e4fbbcd723a799b47cb43e6179139352c5474d83
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2021-10-12 03:29:06 +0200

    Change solution name and update metadata

--- Core_SkinEffects/AssemblyInfo.cs
+++ Core_SkinEffects/AssemblyInfo.cs
@@ -1,33 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("KK_SkinEffects")]
-[assembly: AssemblyDescription("Adds new effects to h scenes and studio, for example bukkake")]
-[assembly: AssemblyCompany("ManlyMarco")]
-[assembly: AssemblyProduct("KK_SkinEffects")]
+[assembly: AssemblyTitle("SkinEffects")]
+[assembly: AssemblyDescription("Adds new effects to main game and studio, for example bukkake")]
+[assembly: AssemblyCompany("https://github.com/ManlyMarco/Illusion_SkinEffects")]
+[assembly: AssemblyProduct("Additional Skin Effects mod")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d234a8a5-e3e7-48f6-8bdf-b4592c84ad6c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion(KK_SkinEffects.SkinEffectsPlugin.Version)]
