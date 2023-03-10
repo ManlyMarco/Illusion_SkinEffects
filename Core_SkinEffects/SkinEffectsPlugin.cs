0141fee0ec0af0608904d207939e5e4254487255
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-03-10 13:09:24 +0100

    Version up to 2.1.2

--- Core_SkinEffects/SkinEffectsPlugin.cs
+++ Core_SkinEffects/SkinEffectsPlugin.cs
@@ -16,7 +16,7 @@ namespace KK_SkinEffects
     internal class SkinEffectsPlugin : BaseUnityPlugin
     {
         public const string GUID = "Marco.SkinEffects";
-        public const string Version = "2.1.1";
+        public const string Version = "2.1.2";
 
         internal static new ManualLogSource Logger { get; private set; }
 
